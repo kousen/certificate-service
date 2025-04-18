266a941689a47576bdc8b73fa940b9f8a6764705
Ken Kousen <ken.kousen@kousenit.com>
2025-04-18 16:51:01 -0400

    Add certificate verification QR code with enhanced style and information

--- src/main/java/com/kousen/cert/util/QrCodeUtil.java
+++ src/main/java/com/kousen/cert/util/QrCodeUtil.java
@@ -8,16 +8,66 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public final class QrCodeUtil {
     private QrCodeUtil() {}
 
-    private static final String URL = "https://youtube.com/@TalesFromTheJarSide";
+    private static final String DEFAULT_VERIFICATION_URL = "https://talesofthejarside.com/verify-certificate";
+    
+    /**
+     * Gets the verification URL for a specific certificate
+     * 
+     * @param purchaserName the name of the certificate purchaser
+     * @param bookTitle the title of the book
+     * @return the verification URL
+     */
+    public static String getVerificationUrl(String purchaserName, String bookTitle) {
+        // URL-encode the parameters
+        String encodedName = java.net.URLEncoder.encode(purchaserName, StandardCharsets.UTF_8);
+        String encodedTitle = java.net.URLEncoder.encode(bookTitle, StandardCharsets.UTF_8);
+        
+        // Build verification URL with parameters
+        String formattedDate = java.time.LocalDate.now().format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE);
+        return DEFAULT_VERIFICATION_URL + 
+                "?name=" + encodedName + 
+                "&book=" + encodedTitle +
+                "&date=" + formattedDate;
+    }
 
+    /**
+     * Generates a QR code as a data URI with the default verification URL
+     * 
+     * @param sizePx the size of the QR code in pixels
+     * @return a data URI containing the QR code image
+     */
     public static String dataUri(int sizePx) {
+        return dataUri(DEFAULT_VERIFICATION_URL, sizePx);
+    }
+    
+    /**
+     * Generates a QR code as a data URI with certificate-specific information
+     * 
+     * @param purchaserName the name of the certificate purchaser
+     * @param bookTitle the title of the book
+     * @param sizePx the size of the QR code in pixels
+     * @return a data URI containing the QR code image
+     */
+    public static String dataUri(String purchaserName, String bookTitle, int sizePx) {
+        return dataUri(getVerificationUrl(purchaserName, bookTitle), sizePx);
+    }
+    
+    /**
+     * Generates a QR code as a data URI from a specific URL
+     * 
+     * @param url the URL to encode in the QR code
+     * @param sizePx the size of the QR code in pixels
+     * @return a data URI containing the QR code image
+     */
+    public static String dataUri(String url, int sizePx) {
         try {
-            var bitMatrix = new QRCodeWriter().encode(URL, BarcodeFormat.QR_CODE, sizePx, sizePx);
+            var bitMatrix = new QRCodeWriter().encode(url, BarcodeFormat.QR_CODE, sizePx, sizePx);
             BufferedImage img = MatrixToImageWriter.toBufferedImage(bitMatrix);
             try (var baos = new ByteArrayOutputStream()) {
                 ImageIO.write(img, "PNG", baos);
